b76f2336a477438df85f39f545e4a9067d59164b
Lynn Wolters <lynn.wolters27@gmail.com>
2024-03-28 13:34:38 +0100

    MERGEN

--- scripts/logic/customCursor.js
+++ scripts/logic/customCursor.js
@@ -6,6 +6,10 @@ export class CustomCursor {
         }
         this.cursorTriggerElement = document.querySelectorAll(".cursor-trigger-element")
         this.marqueeElement = document.querySelector(".marquee-wrapper")
+        this.bodyElement = document.querySelectorAll("#lichaam")
+        this.eyeGroups = document.querySelectorAll("#ogen > g")
+        this.openMenu = document.querySelector("nav > button")
+        this.closeMenu = document.querySelector("nav > ul button")
         this.init()
     }
 
@@ -15,25 +19,71 @@ export class CustomCursor {
 
     bindEvents = () => {
         document.addEventListener("mousemove", this.animateCursor)
+
         this.cursorTriggerElement.forEach(triggerElement => {
-            triggerElement.addEventListener("mouseover", () => this.animateMouseOver())
-            triggerElement.addEventListener("mouseout", () => this.animateMouseOut())
+            triggerElement.addEventListener("mouseover", () => this.animateMenuMouseOver())
+            triggerElement.addEventListener("mouseout", () => this.animateMenuMouseOut())
         })
+
+        this.bodyElement.forEach(body => {
+            body.addEventListener("mouseover", () => this.animateBodyMouseOver(body))
+            body.addEventListener("mouseout", () => this.animateBodyMouseOut(body))
+        })
+
+        // Voeg eventlisteners toe voor menu knoppen hover
+        this.openMenu.addEventListener("mouseover", () => this.animateMenuHover())
+        this.openMenu.addEventListener("mouseout", () => this.animateMenuMouseOut())
+
+        this.closeMenu.addEventListener("mouseover", () => this.animateMenuHover())
+        this.closeMenu.addEventListener("mouseout", () => this.animateMenuMouseOut())
     }
 
     animateCursor = ({clientX, clientY}) => {
         this.customCursorElement.style.transform = `translate3d(calc(${clientX}px - 50%), calc(${clientY}px - 50%), 0)`
-    }  
-    
-    animateMouseOver() {
-        this.customCursorElement.classList.add("animate-grow")
-        this.customCursorElement.innerHTML = "Open"
-        this.marqueeElement.style.display = "flex"
+
+        const eyeShiftX = (clientX - window.innerWidth / 2) / 30
+        const eyeShiftY = (clientY - window.innerHeight / 2) / 30
+
+        this.eyeGroups.forEach(eyeGroup => {
+            eyeGroup.setAttribute("transform", `translate(${eyeShiftX}, ${eyeShiftY})`)
+        })
     }
 
-    animateMouseOut() {
+    animateMenuMouseOver() {
+        this.customCursorElement.classList.add("animate-grow")
+        this.customCursorElement.innerHTML = "Open"
+
+        this.marqueeElement.style.display = "block"
+    }
+
+    animateMenuHover() {
+        this.customCursorElement.style.backgroundColor = "#FFFFFF"
+        this.customCursorElement.style.color = "#000000"
+    }
+
+    animateMenuMouseOut() {
         this.customCursorElement.classList.remove("animate-grow")
         this.customCursorElement.innerHTML = ""
-        this.marqueeElement.style.display = "none"
+
+        this.marqueeElement.style.display = "block"
+
+        this.customCursorElement.style.backgroundColor = "var(--black)"
+        this.customCursorElement.style.color = "var(--white)"
+    }
+
+    animateBodyMouseOver(body) {
+        this.bodyElement.forEach(otherBody => {
+            if (otherBody !== body) {
+                otherBody.style.fill = "#F2F2F2"
+            }
+        })
+    }
+
+    animateBodyMouseOut(body) {
+        this.bodyElement.forEach(otherBody => {
+            if (otherBody !== body) {
+                otherBody.style.fill = ""
+            }
+        })
     }
 }
